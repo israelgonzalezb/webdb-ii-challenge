5d054926d6d8fd1e050f6a38af0bee15218800d7
Israel Gonzalez <israelgonzalez@lacroizrules.lan>
2019-08-09 19:06:14 -0700

    knex schema created for cars, api connected to database, get and create endpoints created, tested with postman. MVP complete

---
+++ cars/cars-router.js
@@ -0,0 +1,75 @@
+const express = require("express");
+
+const db = require("../data/db-config.js");
+
+const router = express.Router();
+
+router.use(express.json());
+
+router.get("/", async (req, res) => {
+  try {
+    const cars = await db("cars");
+    res.status(200).json("cars");
+  } catch (err) {
+    router
+      .status(500)
+      .json({ message: "There was an error fetching the data", error: err });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const carData = req.body;
+    const [id] = await db("cars").insert(carData);
+    const newCarEntry = await db("cars").where({ id });
+
+    res.status(201).json(newCarEntry);
+  } catch (err) {
+    res.status(500).json({ message: "Failed to add data", error: err });
+  }
+});
+
+module.exports = router;
+
+/*
+const express = require("express");
+
+const db = require("../data/db-config.js");
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const fruits = await db("fruits");
+    res.json(fruits);
+  } catch (err) {
+    res.status(500).json({ message: "Failed to retrieve fruits" });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const fruit = await db("fruits").where({ id });
+
+    res.json(fruit);
+  } catch (err) {
+    res.status(500).json({ message: "Failed to retrieve fruit" });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const fruitData = req.body;
+    const [id] = await db("fruits").insert(fruitData);
+    const newFruitEntry = await db("fruits").where({ id });
+
+    res.status(201).json(newFruitEntry);
+  } catch (err) {
+    console.log("POST error", err);
+    res.status(500).json({ message: "Failed to store data" });
+  }
+});
+
+module.exports = router;
+*/
